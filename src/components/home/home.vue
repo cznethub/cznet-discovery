585d11a4917923c93b85e920c3029c4a910d676b
Maurier <mauriel.ramirez@gmail.com>
2022-10-12 13:34:36 -0600

    simplify example query

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -7,32 +7,41 @@
     <v-btn color="error">Error</v-btn>
     <v-btn color="success">Success</v-btn>
     <v-btn color="info">Info</v-btn>
+
+    <div v-for="r of fuzzy_search" :key="r._id">{{ r.name }}</div>
   </v-container>
 </template>
 
 <script lang="ts">
   import { Component, Vue } from 'vue-property-decorator'
   import gql from 'graphql-tag'
+
   const Search = require('@/graphql/Search.gql')
 
   @Component({
     name: 'home',
     components: { },
     apollo: {
-      schemaorg: gql`${Search}`,
+      fuzzy_search: {
+        query: gql`${Search}`,
+        variables: { term: ' ', limit: 0 }
+      },
     }
   })
   export default class Home extends Vue {
+    protected fuzzy_search = []
+
     async created() {
-      const response = await this.refetch()
-      console.log(response)
+      console.log('created')
+      await this.refetch()
+      console.log(this)
+      // console.log(response)
     }
 
     async refetch() {
       // Query execution example
-      const query = this.$apollo.queries.schemaorg
-      query.setVariables({ limit: 10, after: null })
-      return query.refetch()
+      const query = this.$apollo.queries.fuzzy_search
+      return query.refetch({ limit: 10, term: 'Temperature' })
     }
   }
 </script>
