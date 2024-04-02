fc4253c44453940a42f15f05c355ebe036c74b15
Maurier <mauriel.ramirez@gmail.com>
2024-04-02 10:52:58 -0600

    add entry point and update env variables

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,4 @@
 export const APP_NAME = process.env.VUE_APP_NAME || "";
-export const SUPPORT_EMAIL = process.env.VUE_APP_SUPPORT_EMAIL || "";
 export const DEFAULT_TOAST_DURATION = 3500;
 
 export const sameRouteNavigationErrorHandler = (e) => {
