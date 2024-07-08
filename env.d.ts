b185acd346b75a2ed3c077608e0a06235cdb5203
Maurier <mauriel.ramirez@gmail.com>
2024-07-08 14:35:19 -0600

    update env variables definitions

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,4 @@
 declare const VITE_APP_NAME: string;
-declare const VITE_APP_SUPPORT_EMAIL: string;
-declare const VITE_APP_HEAP_ANALYTICS_APP_ID: string;
 declare const VITE_APP_GOOGLE_MAPS_API_KEY: string;
 declare const VITE_APP_API_URL: string;
 declare const VITE_APP_DSP_URL: string;
-declare const VITE_APP_VERSION: string;
