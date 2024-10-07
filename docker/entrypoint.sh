187ec11146e398465a4812ca6b74b96b18fe3daa
Jude Sproul <jnsproul@ncsu.edu>
2024-10-07 11:14:23 -0400

    Added additional echo statement for testing

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,12 +7,13 @@ echo "Replacing env constants in JS"
 for file in $ROOT_DIR/assets/index*.js $ROOT_DIR/index.html;
 do
   echo "Processing $file ...";
+  echo "Testing envvars"
   echo "${VITE_APP_NAME}"
   echo "${VITE_APP_GOOGLE_MAPS_API_KEY}"
   echo "${VITE_APP_API_URL}"
   echo "${VITE_APP_DSP_URL}"
   echo "${VITE_APP_HEAP_ANALYTICS_APP_ID}"
-  
+
   sed -i 's|VITE_APP_NAME_PLACEHOLDER|'${VITE_APP_NAME}'|g' $file 
   sed -i 's|VITE_APP_GOOGLE_MAPS_API_KEY_PLACEHOLDER|'${VITE_APP_GOOGLE_MAPS_API_KEY}'|g' $file 
   sed -i 's|VITE_APP_API_URL_PLACEHOLDER|'${VITE_APP_API_URL}'|g' $file 
