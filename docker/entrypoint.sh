20a17bb3029afe1001bc2987efff818e71371752
Jude Sproul <jnsproul@ncsu.edu>
2024-10-07 11:42:30 -0400

    Proved env variables were showing in entrypoint, making changes to sedcommand

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,18 +7,12 @@ echo "Replacing env constants in JS"
 for file in $ROOT_DIR/assets/index*.js $ROOT_DIR/index.html;
 do
   echo "Processing $file ...";
-  echo "Testing envvars"
-  echo "${VITE_APP_NAME}"
-  echo "${VITE_APP_GOOGLE_MAPS_API_KEY}"
-  echo "${VITE_APP_API_URL}"
-  echo "${VITE_APP_DSP_URL}"
-  echo "${VITE_APP_HEAP_ANALYTICS_APP_ID}"
 
-  sed -i 's|VITE_APP_NAME_PLACEHOLDER|'${VITE_APP_NAME}'|g' $file 
-  sed -i 's|VITE_APP_GOOGLE_MAPS_API_KEY_PLACEHOLDER|'${VITE_APP_GOOGLE_MAPS_API_KEY}'|g' $file 
-  sed -i 's|VITE_APP_API_URL_PLACEHOLDER|'${VITE_APP_API_URL}'|g' $file 
-  sed -i 's|VITE_APP_DSP_URL_PLACEHOLDER|'${VITE_APP_DSP_URL}'|g' $file 
-  sed -i 's|VITE_APP_HEAP_ANALYTICS_APP_ID_PLACEHOLDER|'${VITE_APP_HEAP_ANALYTICS_APP_ID}'|g' $file 
+  sed -i "s|VITE_APP_NAME_PLACEHOLDER|${VITE_APP_NAME}|g" $file 
+  sed -i "s|VITE_APP_GOOGLE_MAPS_API_KEY_PLACEHOLDER|${VITE_APP_GOOGLE_MAPS_API_KEY}|g" $file 
+  sed -i "s|VITE_APP_API_URL_PLACEHOLDER|${VITE_APP_API_URL}|g" $file 
+  sed -i "s|VITE_APP_DSP_URL_PLACEHOLDER|${VITE_APP_DSP_URL}|g" $file 
+  sed -i "s|VITE_APP_HEAP_ANALYTICS_APP_ID_PLACEHOLDER|${VITE_APP_HEAP_ANALYTICS_APP_ID}|g" $file 
 done
 
 echo "Starting Nginx"
